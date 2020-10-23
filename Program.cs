9f11e5c5ae6e5df24bb2a29d98cb0fa90ce05715
element2562 <element25668@gmail.com>
2020-10-23 15:51:26 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MoveZerosToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unfilteredArr = new int[] { 0, 2, 4, 5, 0, 0, 2, 5, 3, 4 };
+            int[] filteredArr = MoveZeros(unfilteredArr);
+            foreach(int number in filteredArr)
+            {
+                Console.Write(number);
+            }
+        }
+
+        static int[] MoveZeros(int[] nums)
+        {
+            int[] filteredArr = new int[nums.Length];
+            int counter = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    filteredArr[counter] = nums[i];
+                    counter++;
+                }
+            }
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 0)
+                {
+                    filteredArr[counter] = nums[i];
+                }
+            }
+            return filteredArr;
+        }
+    }
+}
